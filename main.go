9de61be5fc13e6a5e8c378ea64c4b7a0fe81ce8d
Shebut Denis <dshebut@rambler.ru>
2021-08-30 21:30:23 +0300

    Disproportionate interpolation using nearest neighbour method

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"main/basic_operators"
 	"main/transformations"
 	"main/utils"
 	"time"
@@ -11,17 +10,16 @@ import (
 func main() {
 	imageLoader := utils.ImageLoader{}
 	imageSaver := utils.ImageSaver{OutputDir: "test_results/"}
-	testImage1, err := imageLoader.Open("test_images/Lenna_2048.png")
-	testImage2, err := imageLoader.Open("test_images/Lenna_2048.png")
+	testImage1, err := imageLoader.Open("test_images/Lenna_256.png")
 
 	fmt.Println("start interpolation" + time.Now().String())
-	interpolationResult, err := transformations.NearestNeighbor(testImage2, 1)
+	interpolationResult, err := transformations.NearestNeighbor(testImage1, 10, 1)
 	if err != nil {
 
 	}
 	fmt.Println("finish interpolation" + time.Now().String())
-	iTest, err := basic_operators.SubtractImages(testImage1, interpolationResult)
-	err = imageSaver.SavePNG(iTest, "addition_result")
+	///iTest, err := basic_operators.SubtractImages(testImage1, interpolationResult)
+	err = imageSaver.SavePNG(interpolationResult, "interpolation_result")
 	if err != nil {
 
 	}
--- transformations/interpolator.go
+++ transformations/interpolator.go
@@ -6,18 +6,22 @@ import (
 	"main/utils"
 )
 
-func NearestNeighbor(sourceImage image.Image, scale float64) (interpolatedImage *image.RGBA, err error) {
-	if scale < 1 {
+func ProportionalNearestNeighbor(sourceImage image.Image, scale float64) (interpolatedImage *image.RGBA, err error) {
+	return NearestNeighbor(sourceImage, scale, scale)
+}
+
+func NearestNeighbor(sourceImage image.Image, scaleX float64, scaleY float64) (interpolatedImage *image.RGBA, err error) {
+	if scaleX < 1 || scaleY < 1 {
 		return nil, errors.New("method does not support downscaling")
 	}
 	width, height := utils.GetSize(sourceImage)
-	targetWidth := int(float64(width) * scale)
-	targetHeight := int(float64(height) * scale)
+	targetWidth := int(float64(width) * scaleX)
+	targetHeight := int(float64(height) * scaleY)
 
 	interpolatedImage = image.NewRGBA(image.Rect(0, 0, targetWidth, targetHeight))
 	for i := 0; i < targetHeight; i++ {
 		for j := 0; j < targetWidth; j++ {
-			interpolatedImage.Set(j, i, sourceImage.At(int(float64(j)/scale), int(float64(i)/scale)))
+			interpolatedImage.Set(j, i, sourceImage.At(int(float64(j)/scaleX), int(float64(i)/scaleY)))
 		}
 	}
 	return
